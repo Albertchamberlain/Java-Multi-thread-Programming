95e67e610b41b1b23a788177696acd9562cb903c
Albertchamberlain <2476271447@qq.com>
2020-09-25 00:08:48 +0800

    <add>:<chap2>

--- src/src/chap2/MyObject84.java
+++ src/src/chap2/MyObject84.java
@@ -8,19 +8,68 @@ public class MyObject84 {
     {
         try
         {
-            System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
-            Thread.sleep(50000);
+            System.out.println(" = "+Thread.currentThread().getName());
+            Thread.sleep(5000);
             System.out.println("end");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
-class Thread84 extends Thread{
-    private MyObject84 object84;
-    public Thread84(MyObject84 object84)
+
+class Thread84A extends Thread
+{
+    private  MyObject84 object;
+    public Thread84A (MyObject84 myObject84)
     {
         super();
-        this.object84 =object84;
+        this.object = myObject84;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        object.methodA();
+    }
+}
+
+class Thread84B extends Thread
+{
+    private MyObject84 myObject84;
+    public Thread84B(MyObject84 myObject84)
+    {
+        super();
+        this.myObject84 = myObject84;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        myObject84.methodA();
+    }
+}
+
+class Run84
+{
+    public static void main(String[] args) {
+        final MyObject84 myObject84 = new MyObject84();
+        final Thread84A thread84A = new Thread84A(myObject84);
+
+        thread84A.setName("a");
+
+        final Thread84B thread84B = new Thread84B(myObject84);
+        thread84A.setName("b");
+
+        thread84A.start();
+        thread84B.start();
+    }
+}
+
+class MyObject86
+{
+    synchronized public void methodA() throws InterruptedException {
+        System.out.println("Thread.currentThread().getName() = " + Thread.currentThread().getName());
+      Thread.sleep(5000);
+        System.out.println("end");
     }
 }
